c3b85c37ec49ce8cff9fdbd28ba5d19337be308b
Yousopp <yousephagueye@gmail.com>
2022-11-16 11:59:14 -0500

    linked apollo-server-express into typeDefs.js + started creating tyoeDefs

--- Develop/server/schemas/typeDefs.js
+++ Develop/server/schemas/typeDefs.js
@@ -1,3 +1,18 @@
+const { gql } = require("apollo-server-express");
+const typeDefs = gql`
 
+type Query {
+    me: User
+}
+
+type User {
+    _id: ID
+    username: String
+    email: String
+    bookCount: Int
+    savedBooks: [Book]
+}
+
+`
 
 module.exports = typeDefs;
