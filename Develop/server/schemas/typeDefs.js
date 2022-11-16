3c402c12e9d6c3f78fb7ca34a08cc4752f19a4ce
Yousopp <yousephagueye@gmail.com>
2022-11-16 12:23:47 -0500

    completed typeDefs.js

--- Develop/server/schemas/typeDefs.js
+++ Develop/server/schemas/typeDefs.js
@@ -13,6 +13,29 @@ type User {
     savedBooks: [Book]
 }
 
-`
+type Book {
+    bookId: ID
+    authors: [String]
+    description: String
+    title: String
+    image: String
+    link: String
+}
+
+input InputBook {
+    bookId: String
+    authors: [String]
+    title: String
+    description: String
+    image: String
+    link: String
+}
+
+type Mutation {
+    login(email: String, password: String): Auth
+    addUser(username: String!, email: password: String): Auth
+    saveBook(newBook: InputBook): User
+    removeBook(bookId: ID): User
+}`
 
 module.exports = typeDefs;
