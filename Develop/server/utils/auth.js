770dcc3111cc4e066224d435f2f74cda8d0de051
Yousopp <yousephagueye@gmail.com>
2022-11-16 15:28:39 -0500

    completed auth.js

--- Develop/server/utils/auth.js
+++ Develop/server/utils/auth.js
@@ -24,9 +24,10 @@ module.exports = {
       req.user = data;
     } catch {
       console.log('Invalid token');
+      return res.status(400).json({ message: 'invalid token!' });
     }
-
-    return req;
+    next();
+    
   },
   signToken: function({ username, email, _id }) {
     const payload = { username, email, _id };
